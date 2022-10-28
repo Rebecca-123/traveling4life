e16b95b8975a3bd448ec7cc1c4ef97ab41f911dd
Rebecca-123 <rebecca.notw@gmail.com>
2022-10-27 17:51:25 -0700

    lombok is better

--- src/main/java/com/nighthawk/spring_portfolio/mvc/restaurants/Restaurants.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/restaurants/Restaurants.java
@@ -1,7 +1,14 @@
 package com.nighthawk.spring_portfolio.mvc.restaurants;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
 public class Restaurants {
     @Id
@@ -13,50 +20,4 @@ public class Restaurants {
 
     private int five;
     private int one;
-
-    public Restaurants(){
-        this.id = (long)1;
-        this.restr = "In-N-Out";
-        this.five = 0;
-        this.one = 0;
-    }
-
-    public Restaurants(Long id, String restr, int five, int one){
-        this.id = id;
-        this.restr = restr;
-        this.five = five;
-        this.one = one;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public String getRestr(){
-        return restr;
-    }
-
-    public int getFive(){
-        return five;
-    }
-
-    public int getOne(){
-        return one;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public void setRestr(String restr){
-        this.restr = restr;
-    }
-
-    public void setFive(int five){
-        this.five = five;
-    }
-
-    public void setOne(int one){
-        this.one = one;
-    }
 }
